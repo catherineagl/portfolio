4ca0e5bda8737eca828e4f527d996c9777a58508
Catherine Aguilera <catherineaguilera13@gmail.com>
2022-03-02 11:01:50 -0400

    editing footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { FooterContainer } from './FooterElements';
 
 const Footer = () => {
-	return <FooterContainer>All rigths reserved | 2022</FooterContainer>;
+	return (
+		<FooterContainer>Creado por Catherine Aguilera | 2022</FooterContainer>
+	);
 };
 
 export default Footer;
